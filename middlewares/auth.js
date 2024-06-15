20557d9290033f405111d2e406cef233708791fc
Rakshith N <rakshithacchu9625@gmail.com>
2024-06-15 23:49:07 +0530

    message update

--- middlewares/auth.js
+++ middlewares/auth.js
@@ -5,7 +5,7 @@ function authorize(req, res, next) {
     try {
         const authHeader = req.headers['authorization'];
         const token = authHeader && authHeader.split(' ')[1];
-        if (!token) return res.status(401).send('Token required');
+        if (!token) return res.status(401).send('Authorization required');
         jwt.verify(token, process.env.JWT_SECRET, async (err, decoded) => {
             if (err) return res.status(403).send('Invalid or expired token');
             var user = await UserModel.findOne({ where: { username: decoded.username } });
